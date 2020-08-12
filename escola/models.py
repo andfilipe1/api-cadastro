f2e8e20e6f4b715a7b64e93fa2b6577b03fce928
Luiz Filipe <andfilipe1@gmail.com>
2020-08-11 22:38:54 -0300

    Create readme.md

--- escola/models.py
+++ escola/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 import escola.validacpf
+from validate_docbr import CPF
 
 
 
 class Aluno(models.Model):
     nome = models.CharField(max_length=30)
     rg = models.CharField(max_length=9)
-    cpf = models.CharField(), escola.validacpf.ValidaCpf()
+    cpf = models.CharField(max_length=11)
     data_nascimento = models.DateField()
 
     def __str__(self):
